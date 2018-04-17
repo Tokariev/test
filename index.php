ea2ce42779b0e12f96075081a97c07e423b68191
Anton Tyutyunnik <38406175+antontyutyunnik@users.noreply.github.com>
2018-04-17 13:30:24 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+$Name = array ('Миша','Сергей','Вася','Петя','Гриша','Антон');
+$lastName = array ('A' => 'Петров','Б' => 'Михнов','Г' => 'Филимонов',
+                   'В' => 'Сергеев','И' => 'Пупкин','Д' => 'Вальнов',);
+$Name[] = 'Greg';
+        
+
+
+        echo '<pre>';
+          print_r ($Name);
+          print_r ($lastName);
+        echo '</pre>';
+          
+        echo '<pre>';
+          print_r ($Name);
+          print_r ($lastName);
+        echo '</pre>';
+
+?>
